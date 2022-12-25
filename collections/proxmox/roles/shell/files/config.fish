0ebad583b0fa52893a5287c8a376bbecd2d38450
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2022-12-24 18:27:48 -0800

    Fixed fish-shell setup

--- collections/proxmox/roles/shell/files/config.fish
+++ collections/proxmox/roles/shell/files/config.fish
@@ -1,1 +1,9 @@
 set -U fish_user_paths /bin /sbin /usr/bin /usr/sbin /usr/local/bin /usr/local/sbin
+
+# Color scheme from base16-fish
+set -U base16_theme snazzy
+
+# Command prompt configuration
+set -U tide_prompt_icon_connection '·'
+set -U tide_prompt_color_frame_and_connection 'brblack'
+set -U tide_left_prompt_items pwd context git newline character
