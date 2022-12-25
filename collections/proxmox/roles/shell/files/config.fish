7c778c5547a9cac56395e8723b9e52b84c0eeeab
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2022-12-24 18:47:32 -0800

    Updated config.fish to use global variables

--- collections/proxmox/roles/shell/files/config.fish
+++ collections/proxmox/roles/shell/files/config.fish
@@ -1,9 +1,12 @@
-set -U fish_user_paths /bin /sbin /usr/bin /usr/sbin /usr/local/bin /usr/local/sbin
+set -g fish_user_paths /bin /sbin /usr/bin /usr/sbin /usr/local/bin /usr/local/sbin
 
-# Color scheme from base16-fish
-set -U base16_theme snazzy
+if status is-interactive
+    # Color scheme from base16-fish
+    set -g base16_theme snazzy
 
-# Command prompt configuration
-set -U tide_prompt_icon_connection '·'
-set -U tide_prompt_color_frame_and_connection 'brblack'
-set -U tide_left_prompt_items pwd context git newline character
+    # Command prompt configuration
+    set -g tide_prompt_icon_connection '·'
+    set -g tide_prompt_color_frame_and_connection 'brblack'
+    set -g tide_left_prompt_items pwd context git newline character
+    set -g tide_right_prompt_items status cmd_duration jobs time
+end
