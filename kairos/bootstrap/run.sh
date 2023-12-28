5e34e0b364e1d7a936fc413f88faba387d09a1f7
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2023-12-28 15:00:32 -0800

    Setting up 1Password Connect bootstrap on Kairos install

--- kairos/bootstrap/run.sh
+++ kairos/bootstrap/run.sh
@@ -3,4 +3,6 @@ set -ex
 
 K3S_MANIFEST_DIR=${K3S_MANIFEST_DIR:-/var/lib/rancher/k3s/server/manifests}
 mkdir -p "$K3S_MANIFEST_DIR"
-cp ./manifests/k2-bootstrap.yaml "$K3S_MANIFEST_DIR"
+
+kairos-agent render-template -f ./manifests/argocd.yaml > "$K3S_MANIFEST_DIR/argocd.yaml"
+kairos-agent render-template -f ./manifests/1password.yaml > "$K3S_MANIFEST_DIR/1password.yaml"
