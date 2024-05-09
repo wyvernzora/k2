3a313ac27bd5e60cf43fc141bea8d3ed74053f76
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2024-05-08 21:02:42 -0700

    Use manifest export to retrieve built k8s manifest

--- containers/argocd-plugin/scripts/generate.sh
+++ containers/argocd-plugin/scripts/generate.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
-if [ -f "./dist/manifest.k8s.yaml" ]; then
-    cat "./dist/manifest.k8s.yaml"
+MANIFEST=""
+if [ -f "package.json" ]; then
+    PKG_NAME="$(jq -cr '.name' "package.json")"
+    echo "Package: $PKG_NAME" 1>&2
+    MANIFEST="$(npx require-resolve-cli "${PKG_NAME}/manifest")"
+    echo "Manifest: $MANIFEST" 1>&2
+fi
+
+if [ -f "$MANIFEST" ]; then
+    cat "$MANIFEST"
 else
     npx cdk8s-cli synth --stdout
 fi
