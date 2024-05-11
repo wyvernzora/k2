24b7eb2d2479a22ed93e4ecd3332e47a93bbf8ee
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2024-05-10 20:47:49 -0700

    Export K2App from @k2/cdk-lib

--- packages/cdk-lib/src/app.ts
+++ packages/cdk-lib/src/app.ts
@@ -1,10 +1,10 @@
 import * as cdk8s from "cdk8s";
 import { YamlOutputType } from "cdk8s";
 
-interface K2AppProps extends cdk8s.AppProps {}
+export interface K2AppProps extends cdk8s.AppProps {}
 
 export class K2App extends cdk8s.App {
-  constructor(props: K2AppProps) {
+  constructor(props: K2AppProps = {}) {
     super({
       yamlOutputType: YamlOutputType.FILE_PER_APP,
       ...props,
