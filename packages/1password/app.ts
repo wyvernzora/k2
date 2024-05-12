2c86c7e31c8590334a5248299292a3d4da73120a
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2024-05-12 02:02:32 -0700

    Created a HelmChart construct in @k2/cdk-lib

--- packages/1password/app.ts
+++ packages/1password/app.ts
@@ -1,8 +1,33 @@
-import { K2App } from "@k2/cdk-lib";
-import { OnePasswordChart } from "./lib/chart";
+import { K2App, HelmChart } from "@k2/cdk-lib";
+
+const TOLERATE_CONTROL_PLANE = {
+  tolerations: [
+    {
+      key: "CriticalAddonsOnly",
+      operator: "Exists",
+    },
+    {
+      key: "node-role.kubernetes.io/control-plane",
+      operator: "Exists",
+      effect: "NoSchedule",
+    },
+    {
+      key: "node-role.kubernetes.io/master",
+      operator: "Exists",
+      effect: "NoSchedule",
+    },
+  ],
+};
 
 const app = new K2App();
-new OnePasswordChart(app, "1password", {
+new HelmChart(app, "1password", {
   namespace: "k2-core",
+  chart:
+    "helm!https://1password.github.io/connect-helm-charts/connect?version=1.15.0",
+  values: {
+    connect: { ...TOLERATE_CONTROL_PLANE },
+    operator: { create: true, ...TOLERATE_CONTROL_PLANE },
+  },
 });
+
 app.synth();
---
+++ packages/cdk-lib/src/helm.ts
@@ -0,0 +1,53 @@
+import { Construct } from "constructs";
+import { Chart, ChartProps, Helm, HelmProps } from "cdk8s";
+import { basename, dirname } from "path";
+
+/**
+ * Represents a reference to a Helm chart, including its repository and version.
+ * Created from a reference string that looks like the following:
+ *   helm!https://github.com/example/repo/chart-name?version=1.2.3
+ * The rationale for introducing this is to make it easier for dependency management
+ * tools such as Renovate detect and update Helm chart references in what are normally
+ * considered source code files.
+ */
+export class HelmChartRef {
+  readonly repo: string;
+  readonly chart: string;
+  readonly version?: string;
+
+  constructor(value: string) {
+    if (!value.startsWith("helm!")) {
+      throw new Error(`HelmChartRef must start with 'helm!' marker`);
+    }
+    const url = new URL(value.substring(5));
+    this.repo = `${url.protocol}//${url.host}${dirname(url.pathname)}`;
+    this.chart = `${basename(url.pathname)}`;
+    this.version = url.searchParams.get("version") || undefined;
+  }
+}
+
+export interface HelmChartProps extends ChartProps {
+  readonly chart: HelmChartRef | string;
+  readonly values: HelmProps["values"];
+}
+
+/**
+ * HelmChart synthesizes a chart specified by HelmChartRef.
+ */
+export class HelmChart extends Chart {
+  readonly helm: Helm;
+  constructor(scope: Construct, name: string, props: HelmChartProps) {
+    super(scope, name, props);
+    const chartRef =
+      typeof props.chart === "string"
+        ? new HelmChartRef(props.chart)
+        : props.chart;
+
+    this.helm = new Helm(this, "helm", {
+      namespace: this.namespace,
+      releaseName: name,
+      values: props.values,
+      ...chartRef,
+    });
+  }
+}
