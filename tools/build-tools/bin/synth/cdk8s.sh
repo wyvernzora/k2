187f6d6126180e22f2ffa1eadf5fa5b579cdb41e
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2024-05-11 23:26:29 -0700

    Streamlining Helm repo management

--- tools/build-tools/bin/synth/cdk8s.sh
+++ tools/build-tools/bin/synth/cdk8s.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 set -e
 
+# Add helm repos if any are requested
+jq -r '.helm.repos // [] | .[] | [.name, .url] | @tsv' package.json |
+while IFS=$'\t' read -r name url; do
+    helm repo add "$name" "$url"
+done
+
 ts-node app.ts
