469c58dcd8b14c7f3a38d94636a15b0270652231
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2024-05-13 21:07:26 -0700

    Converting @k2/kube-vip into a CDK8s package

---
+++ packages/kube-vip/app.ts
@@ -0,0 +1,52 @@
+import { K2App, HelmChart } from "@k2/cdk-lib";
+
+const app = new K2App();
+new HelmChart(app, "kube-vip", {
+  namespace: "k2-network",
+  chart: "helm:https://kube-vip.github.io/helm-charts/kube-vip@0.6.0",
+  values: {
+    config: {
+      address: "10.10.8.2",
+    },
+    env: {
+      cp_enable: "true",
+      svc_enable: "false",
+      vip_leaderelection: "true",
+    },
+    resources: {
+      limits: {
+        cpu: "200m",
+        memory: "200Mi",
+      },
+      requests: {
+        cpu: "50m",
+        memory: "100Mi",
+      },
+    },
+    affinity: {
+      nodeAffinity: {
+        requiredDuringSchedulingIgnoredDuringExecution: {
+          nodeSelectorTerms: [
+            {
+              matchExpressions: [
+                {
+                  key: "node-role.kubernetes.io/master",
+                  operator: "Exists",
+                },
+              ],
+            },
+            {
+              matchExpressions: [
+                {
+                  key: "node-role.kubernetes.io/control-plane",
+                  operator: "Exists",
+                },
+              ],
+            },
+          ],
+        },
+      },
+    },
+  },
+});
+app.synth();
