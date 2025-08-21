73642c708b840a1ea35965ef7879dc2f22208aa1
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2025-08-20 22:40:18 -0700

    Migrating cert-manager to V3 synth

---
+++ apps/cert-manager/components/cert-manager.ts
@@ -0,0 +1,33 @@
+import { HelmChart } from "@k2/cdk-lib";
+import { Construct } from "constructs";
+import { K2Issuer, K2Certificate } from "@k2/cert-manager";
+import * as OnePassword from "@k2/1password";
+
+export interface CertManagerProps {
+  readonly namespace: string;
+  readonly awsSecretId: string;
+}
+
+export class CertManager extends HelmChart {
+  readonly issuer: K2Issuer;
+  readonly cert: K2Certificate;
+
+  constructor(scope: Construct, name: string, props: CertManagerProps) {
+    super(scope, name, {
+      namespace: props.namespace,
+      chart: "helm:https://charts.jetstack.io/cert-manager@v1.18.2",
+      values: {
+        installCRDs: true,
+        extraArgs: ["--controllers=*"],
+      },
+    });
+
+    this.issuer = new K2Issuer(this, "issuer", {
+      credentials: new OnePassword.K2Secret(this, "aws-credentials", {
+        itemId: "hxitqr6xcco7g2ne3n7m6kkoqa",
+      }),
+    });
+
+    this.cert = new K2Certificate(this, "cert", { issuer: this.issuer });
+  }
+}
--- apps/cert-manager/index.ts
+++ apps/cert-manager/index.ts
@@ -1,3 +1,8 @@
+import { AppResourceFunc, ArgoCDResourceFunc } from "@k2/cdk-lib";
+import { ContinuousDeployment } from "@k2/argocd";
+import { Reflector } from "./components/reflector";
+import { CertManager } from "./components/cert-manager";
+
 /* Export raw CRDs */
 import * as AcmeCRD from "./crds/acme.cert-manager.io";
 import * as MainCRD from "./crds/cert-manager.io";
@@ -11,6 +16,15 @@ export * from "./lib/issuer";
 export * from "./lib/certificate";
 
 /* Export deployment chart factory */
+export const createAppResources: AppResourceFunc = app => {
+  new Reflector(app, "reflector", { namespace: "k2-core" });
+  new CertManager(app, "cert-manager", {
+    namespace: "k2-core",
+    awsSecretId: "hxitqr6xcco7g2ne3n7m6kkoqa",
+  });
+};
 
 /* Export ArgoCD application factory */
-// TODO
+export const createArgoCdResources: ArgoCDResourceFunc = chart => {
+  new ContinuousDeployment(chart, "cert-manager", { namespace: "k2-core" });
+};
