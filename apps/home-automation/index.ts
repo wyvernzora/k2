443303bd2f2abd35bb2d151da36df2389f3f564a
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2025-08-21 18:14:13 -0700

    Migrating home-automation to V3 synth

---
+++ apps/home-automation/index.ts
@@ -0,0 +1,36 @@
+import { ApexDomainContext, AppResourceFunc, ArgoCDResourceFunc, K2Volume } from "@k2/cdk-lib";
+import { HomeAutomation } from "./components";
+import { Size } from "cdk8s";
+import { ContinuousDeployment } from "@k2/argocd";
+
+/* Export deployment chart factory */
+export const createAppResources: AppResourceFunc = app => {
+  const { apexDomain } = ApexDomainContext.of(app);
+  new HomeAutomation(app, "home-automation", {
+    namespace: "home-automation",
+    hostname: `ha.${apexDomain}`,
+    coordinator: "tcp://10.10.229.62:6638",
+    volumes: {
+      mosquitto: {
+        data: K2Volume.replicated({
+          size: Size.gibibytes(1),
+        }),
+      },
+      zigbee2mqtt: {
+        data: K2Volume.replicated({
+          size: Size.gibibytes(1),
+        }),
+      },
+      homeAssistant: {
+        config: K2Volume.replicated({
+          size: Size.gibibytes(1),
+        }),
+      },
+    },
+  });
+};
+
+/* Export ArgoCD application factory */
+export const createArgoCdResources: ArgoCDResourceFunc = chart => {
+  new ContinuousDeployment(chart, "home-automation");
+};
