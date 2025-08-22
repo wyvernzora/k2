b468f2398ab838b4564d9f5821802f544b009115
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2025-08-21 18:33:24 -0700

    Migrating longhorn to V3 synth

--- apps/longhorn/index.ts
+++ apps/longhorn/index.ts
@@ -1,12 +1,31 @@
+import { AppResourceFunc, ArgoCDResourceFunc, HelmChart } from "@k2/cdk-lib";
+import { ContinuousDeployment } from "@k2/argocd";
+import * as Auth from "@k2/auth";
+
 /* Export raw CRDs */
 import * as CRD from "./crds/longhorn.io";
 export const crd = {
   ...CRD,
 };
 
-/* Export higher level constructs */
-
 /* Export deployment chart factory */
+export const createAppResources: AppResourceFunc = app => {
+  new HelmChart(app, "longhorn", {
+    namespace: "k2-storage",
+    chart: "helm:https://charts.longhorn.io/longhorn@1.9.1",
+    values: {
+      ingress: {
+        enabled: true,
+        host: "lh.wyvernzora.io",
+        annotations: {
+          ...Auth.MiddlewareAnnotation,
+        },
+      },
+    },
+  });
+};
 
 /* Export ArgoCD application factory */
-// TODO
+export const createArgoCdResources: ArgoCDResourceFunc = chart => {
+  new ContinuousDeployment(chart, "longhorn", { namespace: "k2-storage" });
+};
