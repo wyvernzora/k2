e16ddceba2903a1f1c489e2f86d3cba69e7e34f0
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2025-08-21 18:33:45 -0700

    Migrating traefik to V3 synth

--- apps/traefik/index.ts
+++ apps/traefik/index.ts
@@ -1,7 +1,12 @@
+import { AppResourceFunc, ArgoCDResourceFunc, HelmChart, Toleration } from "@k2/cdk-lib";
+import { ContinuousDeployment } from "@k2/argocd";
+
 /* Export raw CRDs */
 import * as CRD from "./crds/traefik.io";
 import * as GatewayCRD from "./crds/gateway.networking.k8s.io";
 import * as HubCRD from "./crds/hub.traefik.io";
+import { TlsStore } from "./crds/traefik.io";
+import { K2Certificate } from "@k2/cert-manager";
 
 export const crd = {
   ...CRD,
@@ -9,10 +14,63 @@ export const crd = {
   hub: HubCRD,
 };
 
-/* Export higher level constructs */
-// No constructs
-
 /* Export deployment chart factory */
+export const createAppResources: AppResourceFunc = app => {
+  const chart = new HelmChart(app, "traefik", {
+    namespace: "k2-network",
+    chart: "helm:https://traefik.github.io/charts/traefik@37.0.0",
+    values: {
+      podAnnotations: {
+        "prometheus.io/port": "8082",
+        "prometheus.io/scrape": "true",
+      },
+      providers: {
+        kubernetesCRD: {
+          enabled: true,
+          allowCrossNamespace: true,
+        },
+        kubernetesIngress: {
+          publishedService: {
+            enabled: true,
+          },
+        },
+      },
+      priorityClassName: "system-cluster-critical",
+      tolerations: [...Toleration.ALLOW_CONTROL_PLANE, ...Toleration.ALLOW_CRITICAL_ADDONS_ONLY],
+      service: {
+        ipFamilyPolicy: "PreferDualStack",
+      },
+      ingressRoute: {
+        dashboard: {
+          enabled: true,
+          matchRule: "Host(`k2.wyvernzora.io`) && PathPrefix(`/traefik`)",
+          entryPoints: ["web", "websecure"],
+          middlewares: [
+            {
+              name: "k2-auth-authelia@kubernetescrd",
+            },
+          ],
+        },
+      },
+    },
+  });
+
+  /**
+   * Default TLS Store
+   */
+  new TlsStore(chart, "default", {
+    metadata: {
+      name: "default",
+    },
+    spec: {
+      defaultCertificate: {
+        secretName: K2Certificate.Name,
+      },
+    },
+  });
+};
 
 /* Export ArgoCD application factory */
-// TODO
+export const createArgoCdResources: ArgoCDResourceFunc = chart => {
+  new ContinuousDeployment(chart, "traefik", { namespace: "k2-network" });
+};
