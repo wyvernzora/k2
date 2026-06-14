796b8dcbbd21d41ce269a0087c067e4470b3a46e
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2026-06-14 00:59:33 -0700

    Allow Pomerium to resolve Postgres service
    Permit DNS lookups for the Nexus Postgres writer service from the Pomerium proxy. Pomerium already has TCP policy to the Postgres pods, but startup fails once Postgres storage is enabled if Cilium blocks the service-name lookup.

--- apps/pomerium/components/network-policy.ts
+++ apps/pomerium/components/network-policy.ts
@@ -3,11 +3,18 @@ import type { Construct } from "constructs";
 import { ApexDomain, K2Chart } from "@k2/cdk-lib";
 import * as postgresql from "@k2/postgresql";
 
-import { egress, EndpointNetworkPolicy, PrivateConnection, tcp } from "../../cilium/lib/netpol/index.js";
+import { egress, EndpointNetworkPolicy, fqdn, PrivateConnection, tcp } from "../../cilium/lib/netpol/index.js";
 import { POMERIUM_IDP_HOST_PREFIX } from "../constants.js";
 import { workloads } from "../index.js";
 
 import { MCP_CLIENT_METADATA_EGRESS_HOSTS } from "./global-config.js";
+
+const POSTGRES_DNS_HOSTS = [
+  fqdn.name("nexus-rw.postgresql.svc"),
+  fqdn.name("nexus-rw.postgresql.svc.cluster.local"),
+  fqdn.name("nexus-rw.postgresql.svc.pomerium.svc.cluster.local"),
+  fqdn.name("nexus-rw.postgresql.svc.svc.cluster.local"),
+];
 
 export class NetworkPolicy extends K2Chart {
   public constructor(scope: Construct, id: string) {
@@ -23,6 +30,10 @@ export class NetworkPolicy extends K2Chart {
     new EndpointNetworkPolicy(this, "controller-oidc-egress", {
       endpoint: proxy,
       egress: [...egress.toDns(...authMetadataHosts), ...egress.toFqdns(authMetadataHosts, tcp(443))],
+    });
+    new EndpointNetworkPolicy(this, "controller-postgresql-dns-egress", {
+      endpoint: proxy,
+      egress: egress.toDns(...POSTGRES_DNS_HOSTS),
     });
     new PrivateConnection(this, "controller-to-postgresql", {
       from: proxy,
