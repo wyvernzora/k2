e10c5c7b79f4b27c3f18be4857133c470f9ee7c2
Denis Luchkin-Zhou <wyvernzora@gmail.com>
2026-07-06 11:39:36 -0700

    Route Blocky DNS through UniFi

--- apps/blocky/index.ts
+++ apps/blocky/index.ts
@@ -5,20 +5,20 @@ import { CoreDnsForward } from "./components/coredns-forward.js";
 import { K8sGateway } from "./components/k8s-gateway/index.js";
 import { NetworkPolicy } from "./components/network-policy.js";
 
-const PUBLIC_DNS_SERVERS = ["1.1.1.1", "9.9.9.9"];
+const UNIFI_DNS_SERVERS = ["10.10.1.1"];
 const BLOCKLIST_URLS = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
 
 export const createAppResources: AppResourceFunc = app => {
   new K8sGateway(app, "k8s-gateway", {
-    publicDnsServers: PUBLIC_DNS_SERVERS,
+    publicDnsServers: UNIFI_DNS_SERVERS,
   });
   new CoreDnsForward(app, "coredns-forward");
   new Blocky(app, "blocky", {
-    publicDnsServers: PUBLIC_DNS_SERVERS,
+    publicDnsServers: UNIFI_DNS_SERVERS,
     blocklistUrls: BLOCKLIST_URLS,
   });
   new NetworkPolicy(app, "network-policy", {
-    publicDnsServers: PUBLIC_DNS_SERVERS,
+    publicDnsServers: UNIFI_DNS_SERVERS,
     blocklistUrls: BLOCKLIST_URLS,
   });
 };
